720f6ad7b8708ace48bb604fad317102af77b5dc
Edionesousa <edione.compras@gmail.com>
2023-02-02 20:46:10 -0300

    novo

--- Curso_dev_web_udemy/script.js
+++ Curso_dev_web_udemy/script.js
@@ -1,6 +1,6 @@
-var a =34;
-var b =33;
-function somar(a,b){
-    return a + b;
-}
-console.log()
+document.querySelector("#calcular").addEventListener("click", function(){
+    let valorA = document.querySelector("#valorA").value;
+    let valorB = document.querySelector("#valorB").value;
+
+    alert(parseInt(valorA)+ parseInt(valorB));
+});
