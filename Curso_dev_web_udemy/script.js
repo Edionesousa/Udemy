3809f080990e81062b16339b2a370c44fe69dcb4
Edionesousa <edione.compras@gmail.com>
2023-02-02 21:26:07 -0300

    novo

--- Curso_dev_web_udemy/script.js
+++ Curso_dev_web_udemy/script.js
@@ -1,6 +1,10 @@
-document.querySelector("#calcular").addEventListener("click", function(){
+document.querySelector("#calcular").addEventListener("click",function(){
     let valorA = document.querySelector("#valorA").value;
     let valorB = document.querySelector("#valorB").value;
-
-    alert(parseInt(valorA)+ parseInt(valorB));
+    if(valorA.length > 0 && valorB.length > 0){
+    alert(parseInt(valorA)+ parseInt(valorB.length > 0))
+    }else{
+    alert("Digite os valores para calcular")
+    }
 });
+
